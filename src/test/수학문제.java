39f2942a676bbdea6d07a8ba357f139c8b09c85d
juesonghyeon <19971alfks@gmail.com>
2025-05-16 20:10:57 +0900

    Feat: EscapeSequence

--- src/test/수학문제.java
+++ src/test/수학문제.java
@@ -2,32 +2,55 @@ package test;
 import java.util.Scanner;
 import java.util.Random;
 
-//public class 수학문제 {
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        Random random = new Random();
-//        int x;
-//        int y;
-//        int i = 0;
-//        int j = 0;
-//        String Ms[] = {"*", "/", "+", "-"};
-//        System.out.println("몇 문제를 푸시겠습니까? :");
-//        int MathProblom = scanner.nextInt();
-//        while(MathProblom == i) {
-//            int Solve = 0;
-//            x = random.nextInt(100);
-//            y = random.nextInt(100);
-//            int z = random.nextInt(4);
-//            System.out.println(x +" " + Ms[z-1] + " " + z + " = ?" );
-//            int MathSolve = scanner.nextInt();
-//            if(Ms[z-1] == "*");
-//
-//            }
-//            i++;
-//
-//
-//
-//
-//        }
-//    }
-//}
+public class 수학문제 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int correctCount = 0;
+        int i = 0;
+        String[] operators = {"*", "/", "+", "-"};
+
+        System.out.print("몇 문제를 푸시겠습니까? : ");
+        int totalQuestions = scanner.nextInt();
+
+        while (totalQuestions > i){
+            int x = random.nextInt(100);
+            int y = random.nextInt(1, 100);  // y는 1~99로 하여 0 방지
+            int operatorIndex = random.nextInt(4);
+            String operator = operators[operatorIndex];
+            int answer = 0;
+
+            switch (operator) {
+                case "*":
+                    answer = x * y;
+                    break;
+                case "/":
+                    answer = x / y;
+                    break;
+                case "+":
+                    answer = x + y;
+                    break;
+                case "-":
+                    answer = x - y;
+                    break;
+                default:
+                    System.out.println("오류 발생");
+                    continue;
+            }
+
+            System.out.println(x + " " + operator + " " + y + " = ?");
+            int userAnswer = scanner.nextInt();
+
+            if (userAnswer == answer) {
+                System.out.println("정답입니다.");
+                correctCount++;
+            } else {
+                System.out.println("오답입니다. 정답은 " + answer + " 입니다.");
+            i++;
+            }
+        }
+
+        System.out.println("총 맞춘 갯수는 : " + correctCount);
+    }
+}
